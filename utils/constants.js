4f1aaa5401da779742192b8144cd12b47ab237c2
Harsh  Vaidya <harshvaidya432004@gmail.com>
2024-11-14 19:35:00 +0530

    adding Menu Api of Swiggy

--- utils/constants.js
+++ utils/constants.js
@@ -4,3 +4,5 @@ export const LOGO_URL = "https://logowik.com/content/uploads/images/free-food-de
 // export const Error_URL = "/kitten-404.jpg"
 export const IMAGE_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.30080&lng=73.20430&restaurantId="
