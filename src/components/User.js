690e90e1e99c6a68ae9bf340acc4737031faddff
Harsh  Vaidya <harshvaidya432004@gmail.com>
2024-11-15 11:38:29 +0530

    Adding github of Harsh Vaidya in about us using UserClass

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,13 @@
-const User = ({name}) => {
-    return (
-        <div className="user-card">
-        <h2>Name: {name}</h2>
-        <h3>Location: Vadodara, Gujarat</h3>
-        <h4>Contact: @harshvaidya04</h4>
-        </div>
-    );
-    };
-    export default User;
+import { useState } from "react";
+
+const User = ({ name }) => {
+  return (
+    <div className="user-card">
+      <h2>Name: {name}</h2>
+      <h3>Location: Vadodara, Gujarat</h3>
+      <h4>Contact: @harshvaidya04</h4>
+    </div>
+  );
+};
+
+export default User;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React from "react";
 class UserClass extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log(props);
-    }
-    render() {
-        return (
-            <div className="user-card">
-            <h2>Name: {this.props.name}</h2>
-            <h3>Location: Vadodara, Gujarat</h3>
-            <h4>Contact: @harshvaidya04</h4>
-            </div>
-        ); 
-    }
-    }
-    export default UserClass;
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: {
+        name: "",
+        location: "",
+        avatar_url: "",
+      },
+      count: 0, // Added count to the state
+    };
+  }
+
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/harsh432004");
+    const json = await data.json();
+    this.setState({
+      userInfo: json,
+    });
+  }
+
+  componentDidUpdate() {
+    // Any additional updates can go here
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  render() {
+    const { name, location, avatar_url } = this.state.userInfo;
+    return (
+      <div>
+        <h2>Name: {name}</h2>
+        <p>Location: {location ? location : "Not Provided"}</p>
+        <img src={avatar_url} alt="User Avatar" />
+      </div>
+    );
+  }
+}
+
+export default UserClass;
