b438faea2fa20d214544c419d39ad3112d89ef85
Harsh  Vaidya <harshvaidya432004@gmail.com>
2024-11-16 13:57:34 +0530

    Adding class components and optimizing the application

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,21 @@
 import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
-    return (
-        <div className="about">
-            <h1>About Us</h1>
-            <h2>We are fastest goods delivery service in Vadodara</h2>
-            <User name={"Harsh Vaidya(function)"} />
-            <UserClass name={"Harsh Vaidya(Class)"}/>
-        </div>
-    );
-};
+import {Component} from "react";
+class About extends Component {
+    constructor(props) {
+        super(props);
+        console.log("Parent Constructor")
+    }
+    render() {
+        console.log("Parent Rendering...")
+        return (
+            <div className="about">
+                <h1>About Us</h1>
+                <h2>We are fastest goods delivery service in Vadodara</h2>
+                <User name={"Harsh Vaidya(function)"} />
+                <UserClass name={"Harsh Vaidya(Class2)"}/>
+            </div>
+        );
+    }
+}
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
@@ -7,12 +8,16 @@ class UserClass extends React.Component {
         name: "",
         location: "",
         avatar_url: "",
+        followers: 0,
+        following: 0,
+        public_repos: 0
       },
-      count: 0, // Added count to the state
     };
+    console.log("Child constructor");
   }
 
   async componentDidMount() {
+    // In react, when we make API calls, we need to render the component once, make the API call, and fill the component with data
     const data = await fetch("https://api.github.com/users/harsh432004");
     const json = await data.json();
     this.setState({
@@ -21,23 +26,32 @@ class UserClass extends React.Component {
   }
 
   componentDidUpdate() {
-    // Any additional updates can go here
-  }
+      console.log("Component updated");
+  
+    }
+  
 
   componentWillUnmount() {
-    console.log("componentWillUnmount");
+    // clearInterval(this.timer)
+    console.log("Component will unmount");
   }
 
   render() {
-    const { name, location, avatar_url } = this.state.userInfo;
+    const { name, location, avatar_url, followers, following, public_repos } = this.state.userInfo;
+    console.log("Child Render");
     return (
       <div>
         <h2>Name: {name}</h2>
         <p>Location: {location ? location : "Not Provided"}</p>
-        <img src={avatar_url} alt="User Avatar" />
+        <img src={avatar_url} alt="User Avatar" style={{ width: "150px", borderRadius: "50%" }} />
+        <h3>Followers: {followers}</h3>
+
       </div>
     );
   }
 }
 
 export default UserClass;
+
+//  we need to unmount intervals after component did mount to prevent from being called again and again
+// to clear the interval use clear interval in componentWillUnmount
